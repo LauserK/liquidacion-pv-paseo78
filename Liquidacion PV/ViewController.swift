9e403d14d99ec2486324ebecbdd9b3fa8207fa2d
lauserk <kildarealauser@gmail.com>
2018-04-28 11:22:01 -0400

    Initial Commit

---
+++ Liquidacion PV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Liquidacion PV
+//
+//  Created by Macbook on 28/4/18.
+//  Copyright © 2018 Grupo Paseo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
